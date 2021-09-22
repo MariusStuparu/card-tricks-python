40e844558309060e9fce761e8d2a31543963d1de
Marius Stuparu <marius@mariusstuparu.com>
2021-09-23 00:25:15 +0300

    main test runner

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import unittest
+
+from tests.test_card import CardTest
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+"""Make sure local imports work in all OS`"""
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
