31b516de5140c16c5a4ccb0fbc24624021269d8d
Marius Stuparu <marius@mariusstuparu.com>
2021-09-23 11:35:46 +0300

    require python 3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Card tricks game written in Python
+# Card tricks game written in Python 3
 
 This is a simple proof of concept / muscles flexing. Take it as it is.
+
+At least Python 3.1 is required
--- main.py
+++ main.py
@@ -2,58 +2,63 @@
 
 from random import randint
 from time import sleep
+import sys
 
 from Deck.Deck import Deck
 
+
 def main():
-    print('The magician')
+    if sys.version_info[0] == 3 and sys.version_info[1] >= 1:
+        print('The magician')
 
-    """Set up"""
-    deckPositions = ['top', 'bottom', 'anywhere']
+        """Set up"""
+        deckPositions = ['top', 'bottom', 'anywhere']
 
-    deck = Deck()
+        deck = Deck()
 
-    """Run"""
-    while input('Do you want me to show you a card trick? (y/n) ') == 'y':
-        print('Shuffle the deck')
-        deck.shuffleDeck()
-        print()
-        sleep(2)
+        """Run"""
+        while input('Do you want me to show you a card trick? (y/n) ') == 'y':
+            print('Shuffle the deck')
+            deck.shuffleDeck()
+            print()
+            sleep(2)
 
-        randomPos1 = deckPositions[randint(0, 2)]
-        print(f'Take one card from {randomPos1}')
-        card1 = deck.takeOneCard(fromWhere=randomPos1)
-        print()
-        sleep(2)
+            randomPos1 = deckPositions[randint(0, 2)]
+            print(f'Take one card from {randomPos1}')
+            card1 = deck.takeOneCard(fromWhere=randomPos1)
+            print()
+            sleep(2)
 
-        randomPos2 = deckPositions[randint(0, 2)]
-        print(f'Put it back {randomPos2}')
-        deck.putCardBack(card1, randomPos2)
-        print()
-        sleep(2)
+            randomPos2 = deckPositions[randint(0, 2)]
+            print(f'Put it back {randomPos2}')
+            deck.putCardBack(card1, randomPos2)
+            print()
+            sleep(2)
 
-        print('Shuffle the deck again')
-        deck.shuffleDeck()
-        print()
-        sleep(2)
+            print('Shuffle the deck again')
+            deck.shuffleDeck()
+            print()
+            sleep(2)
 
-        randomPos3 = deckPositions[randint(0, 2)]
-        print(f'Take one card from {randomPos3}')
-        card2 = deck.takeOneCard(fromWhere=randomPos3)
-        print()
-        sleep(2)
+            randomPos3 = deckPositions[randint(0, 2)]
+            print(f'Take one card from {randomPos3}')
+            card2 = deck.takeOneCard(fromWhere=randomPos3)
+            print()
+            sleep(2)
 
-        print('Is it the same card as before?')
-        print(f'First card: {card1.getCardFull()}')
-        print(f'Second card: {card2.getCardFull()}')
-        print()
-        if card1.getCardFull() == card2.getCardFull():
-            print('Am I good or what?')
-        else:
-            print('Look at this note I have written yesterday.')
+            print('Is it the same card as before?')
+            print(f'First card: {card1.getCardFull()}')
+            print(f'Second card: {card2.getCardFull()}')
+            print()
+            if card1.getCardFull() == card2.getCardFull():
+                print('Am I good or what?')
+            else:
+                print('Look at this note I have written yesterday.')
 
-        deck.putCardBack(card2, randomPos2)
-        print()
+            deck.putCardBack(card2, randomPos2)
+            print()
+    else:
+        print('Wrong Python version')
 
 
 if __name__ == '__main__':
